b6c87fdf3abfa99ee5741f26183ecc114ee6d461
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-12-12 17:28:33 +0300

    connect database

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect();
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Database is connected...");
   } catch (err) {
-    console.log(err);
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
   }
 };
 
