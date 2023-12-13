fdc89adad42c2dcc99ab397314b56b279d47ed69
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-12-13 06:15:40 +0300

    API & endpoint

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 
+import userRoutes from "./routes/userRoutes";
+
 dotenv.config();
 connectDB();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.get("/", (rej, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
