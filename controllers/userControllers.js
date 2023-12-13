08fd2d85d8c0a40809879eeb384a3c5ef0646b59
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-12-13 06:14:38 +0300

    controllers created

---
+++ controllers/userControllers.js
@@ -0,0 +1,33 @@
+import User from "../models/User";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User have already registered" });
+    }
+
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      _id: user._id,
+      avatar: user.avatar,
+      name: user.name,
+      email: user.email,
+      verified: user.verified,
+      admin: user.admin,
+      token: await user.generateJWT(),
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+export { registerUser };
