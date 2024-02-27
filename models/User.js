05cf1dea728420423fc44efd64161b71c6c218da
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-02-27 23:30:17 +0300

    Fix generateJWT method in UserSchema and update jsonwebtoken version

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ UserSchema.pre("save", async function (next) {
   return next();
 });
 
-UserSchema.method.generateJWT = async function () {
+UserSchema.methods.generateJWT = async function () {
   return await sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
