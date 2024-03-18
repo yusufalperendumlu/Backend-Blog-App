45c9c9ef6194c94578fa9f50e2e64f20204cbf32
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-03-19 02:50:59 +0300

    Add updatePost function to postControllers.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost } from "../controllers/postControllers";
+import { createPost, updatePost } from "../controllers/postControllers";
 
 import { authGuard, adminGuard } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/", authGuard, adminGuard, createPost);
+router.put("/:slug", authGuard, adminGuard, updatePost);
 
 export default router;
